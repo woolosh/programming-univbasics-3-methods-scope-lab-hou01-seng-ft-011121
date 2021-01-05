f1b0eb34d7895e4759cf87c807ea5e14aca24c0a
Joshua Morales <joshuapmorales@gmail.com>
2021-01-05 08:13:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,16 @@
 
 def mario
+<<<<<<< HEAD
   $mario_phrase = "It's-a me, Mario!"
   puts "#{$mario_phrase}"
 
 return $mario_phrase
+=======
+  $phrase = "It's-a me, Mario!"
+  puts "#{$phrase}"
+
+puts mario
+>>>>>>> c9e67b0c6f1bf9b2cf3428c20e86b89d0c43ee25
 
 end
 
@@ -12,12 +19,17 @@ def toadstool
   $status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts "#{$status}"
 
+<<<<<<< HEAD
 return $status
+=======
+puts toadstool
+>>>>>>> c9e67b0c6f1bf9b2cf3428c20e86b89d0c43ee25
 
 end
 
 
 def link 
+<<<<<<< HEAD
   $link_phrase = "It's Dangerous To Go Alone! Take This."
   puts "#{$link_phrase}"
 
@@ -34,5 +46,11 @@ def all_phrases
 return $mario_phrase
 return $status
 return $link_phrase
+=======
+  $phrase = "It's Dangerous To Go Alone! Take This!"
+  puts "#{$phrase}"
+
+puts link 
+>>>>>>> c9e67b0c6f1bf9b2cf3428c20e86b89d0c43ee25
 
 end
